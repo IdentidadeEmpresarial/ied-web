3dde28432d894cacb75002b5a4c54dd3cbd3b967
costa43 <thiagotcc@gmail.com>
2022-11-02 22:19:28 -0300

    Defined credentials list on dashboard.

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,5 +1,7 @@
-
 import Link from 'next/Link'
+import {useState, useEffect} from 'react'
+import Web3 from 'web3'
+import CredentialABI from '../lib/credential-abi.json'
 
 function Header({ title }) {
   return <h1>{title ? title : "CDiD"}</h1>
@@ -7,16 +9,122 @@ function Header({ title }) {
 
 export default function Dashboard(App) {
 
+  const iWeb3 = new Web3(process.env.NEXT_PUBLIC_RPC_URL);
+  const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
+  const contract = new iWeb3.eth.Contract(CredentialABI, contractAddress)
+
+  const [ethEnabled, setEthEnabled] = useState(false);
+  const [address, setAddress] = useState(null);
+  const [issuers, setIssuers] = useState([
+    {
+      id: 1,
+      name: 'Receita',
+      credentialTypes: [
+        {
+          id: 1,
+          name: 'CNPJ - Inscrição no Cadastro Nacional da Pessoa Jurídica'
+        }, {
+          id: 2,
+          name: 'Regularidade com a Fazenda Nacional'
+        }
+      ]
+    }
+  ]);
+
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.request({ method: 'eth_requestAccounts' })
+        .then(() => {
+          window.web3 = new Web3(window.ethereum);
+          setEthEnabled(true);
+          window.web3.eth.getAccounts().then(accounts => {
+            setAddress(accounts[0]);
+            listCredentials(accounts[0]);
+          })
+        })
+    }
+    setEthEnabled(false);
+  }, []);
+
+  async function decrypt(encryptedMessage) {
+    return ethereum
+      .request({
+        method: 'eth_decrypt',
+        params: [encryptedMessage, address],
+      }).catch((error) => {
+        console.log(error.message);
+      });
+  }
+
+  async function decryptCredentials() {
+    credentials.forEach(async credential => {
+          if (credential.data && credential.data.length > 10) {
+            await decrypt(credential.data)
+              .then(decryptedMessage => {
+                credential.decryptedData = decryptedMessage;
+                set(credentials.slice())
+              }).catch(error => console.log(error));
+          }
+    });
+  }
+
+  async function listCredentials(address) {
+    console.log(address);
+    contract.methods.getAllTokensByOwner(address).call()
+      .then(async (tokensIds) => {
+        tokensIds.map(async (tokenId) => {
+          await contract.methods.attributes(tokenId).call()
+            .then(async (attributes) => {
+                  credential = {
+                      id: tokenId,
+                      subjectId: attributes.subjectId,
+                      type: attributes.objectType,
+                      data: attributes.data,
+                      dataHash: attributes.dataHash,
+                      dataKey: attributes.dataKey
+                    }
+                  issuers.forEach(issuer => {
+                    const credentialType = issuer.credentialTypes.map(type => type.name === credential.type);
+                    credentialType.holderCredential = credential;
+                  });
+                  setIssuers(issuers.slice())
+            });
+        });
+      })
+  }
+
   return (
     <>
-      <div className="header hidden">
+      <div className="header">
         <div className="header-left">
-            <img loading="lazy" src="/images/logo.png" className="logo" />
-            <div className="header-title">Identidade Digital</div>
+          <img loading="lazy" src="/images/logo.png" className="logo" />
+          <div className="header-title">CDiD</div>
         </div>
         <div className="header-right">
-          <Link href="/dashboard">Começe por aqui</Link>
+          <span>User address: {address}</span>
         </div>
+      </div>
+      <div>Dashboard</div>
+      <div>
+        <ul>{issuers.map((issuer) => (
+          <li key={issuer.id}>
+            <span>{issuer.name}</span>
+            <ul>{issuer.credentialTypes.map((type) => (
+              <li key={type.id}>
+                <div>{type.name}</div>
+                <div>{!type.holderCredential && 
+                  <Link href={{
+                    pathname: "/samples/issuer",
+                    query: {typeId: type.id, typeName: type.name}
+                  }}>Emitir credencial</Link> 
+                 }</div>
+              </li>
+            ))}
+            </ul>
+          </li>
+        ))}
+        </ul>
       </div>
     </>
   )
